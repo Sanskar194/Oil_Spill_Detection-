221e6a6c0a9f421f1e5ed446053c92f9c730cb4d
Sanskar194 <Sanskarsutar005@gmail.com>
2026-01-12 14:32:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ model.eval()
 
 
 =======
->>>>>>> 544f254 (Add deployment files & PyTorch model via LFS)
 def preprocess_image(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.fastNlMeansDenoising(image, None, 10, 7, 21)
@@ -29,7 +28,7 @@ uploaded_file = st.file_uploader("Upload SAR Image", type=["jpg", "png", "jpeg"]
 =======
 uploaded_file = st.file_uploader("Upload SAR Image", type=["jpg", "png", "jpeg"])
 
->>>>>>> 544f254 (Add deployment files & PyTorch model via LFS)
+
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
@@ -37,13 +36,12 @@ if uploaded_file is not None:
 
 =======
 
->>>>>>> 544f254 (Add deployment files & PyTorch model via LFS)
+
     img_tensor = preprocess_image(image)
     with torch.no_grad():
         output = model(img_tensor)
 
 =======
->>>>>>> 544f254 (Add deployment files & PyTorch model via LFS)
     if output.item() > 0.5:
         st.success("Prediction: Oil Spill Detected")
     else:
